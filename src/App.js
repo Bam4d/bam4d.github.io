fc67f2e85a94a6d5202bef8e11b612c5189e0827
Circle CI <chris.bamford@import.io>
2017-08-18 17:25:34 +0100

    progress on getting mathjax to play with react

--- src/App.js
+++ src/App.js
@@ -1,17 +1,43 @@
 import React, { Component } from 'react';
+import MathJax from 'mathjax'
 import MarkdownComponent from './MarkdownComponent';
 import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import testmd from './testmarkdown.md';
+import testmd from './posts/testmarkdown.md';
+//import 'file?name=[name].[ext]!./devtools_page.html';
+
+
+const DEFAULT_OPTIONS = {
+    tex2jax: {
+        inlineMath: []
+    },
+    showMathMenu: false,
+    showMathMenuMSIE: false
+};
 
 class App extends Component {
+
+  componentDidMount() {
+    window.MathJax.Hub.Config(DEFAULT_OPTIONS);
+  }
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <h2>Chris Bamford's Tech Blog</h2>
         </div>
-        <MarkdownComponent markdown={testmd}></MarkdownComponent>
+        <div className="container">
+          <ul class="nav nav-tabs">
+            <li role="presentation" class="active"><a href="#">Home</a></li>
+            <li role="presentation"><a href="#">Profile</a></li>
+            <li role="presentation"><a href="#">Messages</a></li>
+          </ul>
+          <div className="">
+            <MarkdownComponent markdownSrc={testmd}></MarkdownComponent>
+            <MarkdownComponent markdownText="# Here is some text"></MarkdownComponent>
+          </div>
+          </div>
       </div>
     );
   }
--- src/MarkdownComponent.js
+++ src/MarkdownComponent.js
@@ -4,15 +4,47 @@ import mj from 'markdown-it-mathjax'
 
 class MarkdownComponent extends React.Component {
 
-    
     constructor(props) {
         super(props);
         this.md = md().use(mj());
+        this.state = {markdownData: ""}
+    }
+
+    setMarkdown = (markdown) => {
+        console.log("Setting markdown for page");
+        this.setState({markdownData: markdown})
+    }
+
+    componentDidMount(root) {
+        window.MathJax.Hub.Queue(["Typeset",window.MathJax.Hub,root]);
+    }
+
+    componentDidUpdate(props, state, root) {
+        window.MathJax.Hub.Queue(["Typeset",window.Hub,root]);
+    }
+
+    componentWillMount() {
+
+        if(this.props.markdownSrc) {
+            fetch(this.props.markdownSrc)
+            .then((response) => {
+                
+                if(!response.ok) {
+                    return "# Not Found";
+                } else {
+                    return response.text();
+                }
+            }).then((markdownData) => {
+                this.setMarkdown(markdownData);
+            });
+        } else if(this.props.markdownText) {
+            this.setMarkdown(this.props.markdownText);
+        }
     }
 
     render() {
         // pseudo code here, depends on the parser
-        const markdown = this.md.render(this.props.markdown);
+        const markdown = this.md.render(this.state.markdownData);
         return <div dangerouslySetInnerHTML={{__html:markdown}} />;
     }
 };
