ae91887cf02616de4521da00aedcf2380732070b
Circle CI <chris.bamford@import.io>
2017-08-24 10:20:49 +0100

    Updates

---
+++ static/js/2.76609cc9.chunk.js
@@ -0,0 +1,2 @@
+webpackJsonp([2],{430:function(t,a,b){t.exports=b.p+"static/media/aboutme.306bf7bd.md"}});
+//# sourceMappingURL=2.76609cc9.chunk.js.map
--- static/js/2.e3829d4f.chunk.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([2],{427:function(t,a,b){t.exports=b.p+"static/media/aboutme.306bf7bd.md"}});
-//# sourceMappingURL=2.e3829d4f.chunk.js.map
